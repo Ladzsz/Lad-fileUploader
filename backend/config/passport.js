9bd72cee62f9d51100f6b08e3024f119dd3290a2
Ladzsz <londonmason602@gail.com>
2026-09-15 15:27:58 -0400

    set up passport strategy and config

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,16 +1,51 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
 module.exports = (passport) => {
-  // future strategy configuration will go here
-  
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password',
+      },
+      async (email, password, done) => {
+        try {
+          const user = await prisma.user.findUnique({
+            where: { email: email },
+          });
+
+          if (!user) {
+            return done(null, false, {
+              message: 'Incorrect email or password.',
+            });
+          }
+
+          const isMatch = await bcrypt.compare(password, user.passwordHash);
+
+          if (!isMatch) {
+            return done(null, false, {
+              message: 'Incorrect email or password.',
+            });
+          }
+
+          return done(null, user);
+        } catch (error) {
+          return done(error);
+        }
+      }
+    )
+  );
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
   passport.deserializeUser(async (id, done) => {
-    // prisma client will be used here later
-
-    // For now, just return the ID
-    done(null, { id });
+    User.prisma.findUnique({ where: { id } }, function(err, user) {
+        done(err, user);
+    });
   });
 };
-
